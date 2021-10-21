da0d913587074ca731ba8b6cc3dec1c10051c728
Orxan2 <orkhanaib@code.edu.az>
2021-10-22 01:03:06 +0400

    ToysSectionAdded

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,21 @@
-$('.owl-carousel').owlCarousel({
-    margin:10,
-    loop:true,
-    autoWidth:true,
-    items:4
-})
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        margin: 10,
+        loop: true,
+        autoWidth: true,
+        items: 4
+    });
+
+    $(".toy-card").each(function (indexInArray, valueOfElement) {
+        $(valueOfElement).on('mouseenter', function () {
+            console.log(valueOfElement.children[0].children[1]);
+            $(valueOfElement.children[0].children[1]).slideToggle();
+        });
+        $(valueOfElement).on('mouseleave', function () {
+            console.log(valueOfElement.children[0].children[1]);
+            $(valueOfElement.children[0].children[1]).slideToggle();
+        });
+    });
+
+});
+
